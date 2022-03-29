ed8b8bb72bdab8c2d17035bfc303ce02bcb78d45
Carlos Junior - Wine <rs.carlosjunior@gmail.com>
2022-03-29 00:41:20 -0300

    update axios config

--- src/services/register.tsx
+++ src/services/register.tsx
@@ -1,20 +1,13 @@
 import axios from 'axios'
 export const Lead = (userParams: any) => {
-
-   let axiosConfig = {
+  let axiosConfig = {
     headers: {
-      'Content-Type': 'application/json;charset=UTF-8',
-      'Accept': 'application/json',
+      'Content-Type': 'application/json',
       'api-token':
         '93dd8831a9a9e5763432e625c76ec48b78d0349d4c4b92e0917aaebc59cb7d5d33d71edf',
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': '*',
-      'Access-Control-Request-Headers':'origin, x-requested-with',
-      'Origin': '*',
-      'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
-      'Access-Control-Allow-Headers': 'Accept',
-      'Access-Control-Max-Age': '86400',
-      'Connection': 'keep-alive'
+      'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT',
+      'Access-Control-Max-Age': '86400'
     }
   }
 
