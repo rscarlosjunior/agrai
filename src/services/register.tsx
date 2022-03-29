9c3febecfd1ecaadc82e8a0b0816a5b7ddcc7b78
Carlos Junior - Wine <rs.carlosjunior@gmail.com>
2022-03-29 00:14:35 -0300

    Update register.tsx

--- src/services/register.tsx
+++ src/services/register.tsx
@@ -3,8 +3,14 @@ export const Lead = (userParams: any) => {
 
    let axiosConfig = {
     headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+      'Accept': 'application/json',
       'api-token':
         '93dd8831a9a9e5763432e625c76ec48b78d0349d4c4b92e0917aaebc59cb7d5d33d71edf',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': '*',
+      'Access-Control-Allow-Methods': 'GET,POST,PUT',
+      'Access-Control-Allow-Headers': 'Accept'
     }
   }
 
