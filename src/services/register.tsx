f31fc2a8485eccac6e1af2d775c7eddd08b7dd3c
Carlos Junior - Wine <rs.carlosjunior@gmail.com>
2022-03-29 00:52:35 -0300

    Update register.tsx

--- src/services/register.tsx
+++ src/services/register.tsx
@@ -6,7 +6,7 @@ export const Lead = (userParams: any) => {
       'api-token':
         '93dd8831a9a9e5763432e625c76ec48b78d0349d4c4b92e0917aaebc59cb7d5d33d71edf',
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT',
+      'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS, PUT',
       'Access-Control-Max-Age': '86400'
     }
   }
