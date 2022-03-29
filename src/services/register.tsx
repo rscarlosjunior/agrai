a7e5cd932ab3fdd5a59d3e909136cce3b1a1c208
Carlos Junior - Wine <64548465+globalsysroberto@users.noreply.github.com>
2022-03-29 11:40:58 -0300

    fix: request headers

--- src/services/register.tsx
+++ src/services/register.tsx
@@ -5,9 +5,6 @@ export const Lead = (userParams: any) => {
       'Content-Type': 'application/json',
       'api-token':
         '93dd8831a9a9e5763432e625c76ec48b78d0349d4c4b92e0917aaebc59cb7d5d33d71edf',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS, PUT',
-      'Access-Control-Max-Age': '86400'
     }
   }
 
